8796eeceaea5bbb17b2da782b41b207c6c20bd3f
jayspatel21 <jayspatel2153@gmail.com>
2025-01-21 14:40:56 +0530

    change in frontend folder

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,6 @@ console.log("Hello, World!");
 // If you want to display it in a browser
 document.addEventListener('DOMContentLoaded', () => {
     const element = document.createElement('h1');
-    element.textContent = 'Hello, World!!!!!!';
+    element.textContent = 'Hello, World!!!!!!!!!!!!!!';
     document.body.appendChild(element);
 });
